576794404d78d6dddc3525d7ba05a94ff805c4dd
Peter Kosek <peter.kosek@bluwbee.com>
2023-02-08 00:30:31 +0000

    Working v; single transfer per connection only.

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,79 @@
 import Head from "next/head";
 import { useEffect, useState, useRef } from "react";
-
+//filters so that we don't detect unnecessary usb devices in a system
 const filters = [
-  // { vendorId: 0x2fe3, productId: 0x0100 },
-  // { vendorId: 0x2fe3, productId: 0x00a },
-  // { vendorId: 0x8086, productId: 0xf8a1 },
+  { vendorId: 0x2fe3, productId: 0x0100 },
+  { vendorId: 0x2fe3, productId: 0x00a },
+  { vendorId: 0x8086, productId: 0xf8a1 },
 ];
 
 export default function Home() {
-  //States
-  const [senderArea, setSenderArea] = useState("Type a message here:");
+  //Global components
+  const [senderArea, setSenderArea] = useState("Type a message here: ");
   const [reciverArea, setReciverArea] = useState("");
-  const [endpointIn, setEndpointIn] = useState();
-  const [endpointOut, setEndpointOut] = useState();
+  //const [endpointIn, setEndpointIn] = useState(0);
+  //const [endpointOut, setEndpointOut] = useState(0);
+  var endpointIn, endpointOut;
 
-  //pointer equivalent
   let portRef = useRef();
 
-  //Functions
-
-  //This function allows for establising connection with a usb device.
+  ///////////////////////////////////////////////////////////////////
+  //
+  //This function allows for establising connection with an usb device.
+  //
+  ///////////////////////////////////////////////////////////////////
   const Connect = async () => {
     try {
-      // setDevice(request);
       let request = await navigator.usb.requestDevice({ filters: filters });
       portRef.current = request;
-
+      console.log(portRef.current);
       //endpoints setup
-      let endpointIn =
-        request.configuration.interfaces[2].alternate.endpoints.filter(
-          (endpoint) => endpoint.direction !== "out"
-        )[0];
-      setEndpointIn(endpointIn);
-      let endpointOut =
-        request.configuration.interfaces[2].alternate.endpoints.filter(
-          (endpoint) => endpoint.direction !== "in"
-        )[0];
-      setEndpointOut(endpointOut);
-
-      await request.open();
-      if (request.configuration === null) await request.selectConfiguration(1);
-      await request.claimInterface(
-        request.configuration.interfaces[2].interfaceNumber
-      );
-      Listen();
-      console.log(response);
+      endpointIn =
+        portRef.current.configuration.interfaces[0].alternate.endpoints[0]
+          .endpointNumber;
+      endpointOut =
+        portRef.current.configuration.interfaces[0].alternate.endpoints[1]
+          .endpointNumber;
+      // portRef.current.configuration.interfaces[0].alternate.endpoints.filter(
+      //   (endpoint) => endpoint.direction !== "in"
+      // )[0];
+      //setEndpointOut(endpoint);
+      //console.log(endpointIn);
+      //claiming an interface
+      await portRef.current.open();
+      if (portRef.current.configuration === null)
+        await portRef.current.selectConfiguration(1);
+      await portRef.current.claimInterface(0);
+      //request.configuration.interfaces[2].interfaceNumber
+      Listen(); //start listening for incoming transfers
     } catch (error) {
       portRef.current.close();
-      console.log(error, "error");
+      console.log(error, "ERROR - connection failed");
     }
   };
 
+  ///////////////////////////////////////////////////////////////////
+  //
   //A function for listening for incoming WebUSB tranfers.
   //This is a recursive function as it has to always listen for
-  //incoming transfers. It is in effect a Receive function.
+  //incoming transfers. It is in effect a text receiving function.
+  //
+  ///////////////////////////////////////////////////////////////////
   let Listen = () => {
     if (!portRef.current) return;
+    portRef.current.transferIn(endpointIn, 64).then((result) => {
+      setReciverArea(new TextDecoder().decode(result.data));
 
-    requestRef.current
-      .transferIn(endpointIn.endpointNumber, 64)
-      .then((data) => {
-        setReciverArea(new TextDecoder().decode(data));
-        Listen();
-      });
+      Listen(); //recursion
+    });
   };
 
+  ///////////////////////////////////////////////////////////////////
+  //
   //A function for reading a binary file. Takes the input of the
   //"choose file" button.
+  //
+  ///////////////////////////////////////////////////////////////////
   const ReadFile = async (input) => {
     let file = input.target.files[0];
     let reader = new FileReader();
@@ -81,19 +88,22 @@ export default function Home() {
     console.log(input);
   };
 
+  ///////////////////////////////////////////////////////////////////
+  //
   //This function allows for sending data to the connected
   //usb device. Data has to be of string type.
+  //
+  ///////////////////////////////////////////////////////////////////
   const Send = (data) => {
     if (!portRef.current) return;
-    console.log(portRef);
-    console.log("sending to serial:" + data.length);
     if (data.length === 0) return;
+    console.log("sending to serial:" + data.length);
     console.log("sending to serial: [" + data + "]\n");
 
     //converting the data into to utf-8 format
     let view = new TextEncoder().encode(data);
     console.log(view);
-    requestRef.current.transferOut(endpointOut.endpointNumber, view);
+    portRef.current.transferOut(endpointOut, view);
   };
 
   //Rendered wabpage contents/ DOM structure
@@ -101,7 +111,6 @@ export default function Home() {
     <>
       <Head>
         <title>Delta WebUSB App</title>
-        <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -128,11 +137,7 @@ export default function Home() {
             flexDirection: "row",
             marginTop: 150,
           }}
-        >
-          <button id="submit" onClick={() => Send(senderArea)}>
-            Send
-          </button>
-        </div>
+        ></div>
         <div
           style={{
             display: "flex",
@@ -141,13 +146,18 @@ export default function Home() {
             gap: 15,
           }}
         >
-          <label htmlFor="editor">Sender: </label>
-          <textarea
-            id="editor"
-            cols={30}
-            onChange={(e) => setSenderArea(e.target.value)}
-            value={senderArea}
-          ></textarea>
+          <button id="submit" type="submit" onClick={() => Send(senderArea)}>
+            Send
+          </button>
+          <label>
+            Sender
+            <textarea
+              name="writeMessage"
+              cols={30}
+              onChange={(e) => setSenderArea(e.target.value)}
+              value={senderArea}
+            ></textarea>
+          </label>
           <label htmlFor="output"> Receiver: </label>
           <textarea
             id="output"
