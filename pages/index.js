ab341788626b1b57a488ec1345deb6322ff3da48
Peter Kosek <peter.kosek@bluwbee.com>
2023-02-17 11:54:43 +0000

    Removed demo functionality, started target design

--- pages/index.js
+++ pages/index.js
@@ -9,69 +9,74 @@ const filters = [
 
 export default function Home() {
   //Global components
-  const [senderArea, setSenderArea] = useState("Type a message here: ");
-  const [reciverArea, setReciverArea] = useState("");
-  //const [endpointIn, setEndpointIn] = useState(0);
-  //const [endpointOut, setEndpointOut] = useState(0);
-  var endpointIn, endpointOut;
+  const [productName, setProductName] = useState("");
+  const [manufacturer, setManufacturer] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
 
   let portRef = useRef();
 
   ///////////////////////////////////////////////////////////////////
   //
-  //This function allows for establising connection with an usb device.
+  // This function allows for a prompt with avaiable usb devices to
+  // pop up and to request connection with one of them.
   //
   ///////////////////////////////////////////////////////////////////
-  const Connect = async () => {
+  const GetDevices = async () => {
     try {
       let request = await navigator.usb.requestDevice({ filters: filters });
       portRef.current = request;
       console.log(portRef.current);
-      //endpoints setup
-      endpointIn =
-        portRef.current.configuration.interfaces[0].alternate.endpoints[0]
-          .endpointNumber;
-      endpointOut =
-        portRef.current.configuration.interfaces[0].alternate.endpoints[1]
-          .endpointNumber;
-      // portRef.current.configuration.interfaces[0].alternate.endpoints.filter(
-      //   (endpoint) => endpoint.direction !== "in"
-      // )[0];
-      //setEndpointOut(endpoint);
-      //console.log(endpointIn);
+      Connect();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  ///////////////////////////////////////////////////////////////////
+  //
+  // This function allows for establising connection with an usb device.
+  //
+  ///////////////////////////////////////////////////////////////////
+  const Connect = async () => {
+    try {
       //claiming an interface
       await portRef.current.open();
       if (portRef.current.configuration === null)
         await portRef.current.selectConfiguration(1);
       await portRef.current.claimInterface(0);
-      //request.configuration.interfaces[2].interfaceNumber
+      setProductName(portRef.current.productName);
+      setManufacturer(portRef.current.manufacturerName);
+      setSerialNumber(portRef.current.serialNumber);
+      //display data ab the connected device below connect button
+
       Listen(); //start listening for incoming transfers
     } catch (error) {
       portRef.current.close();
-      console.log(error, "ERROR - connection failed");
+      console.log(error, "ERROR - failed to claim interface");
     }
   };
 
   ///////////////////////////////////////////////////////////////////
   //
-  //A function for listening for incoming WebUSB tranfers.
-  //This is a recursive function as it has to always listen for
-  //incoming transfers. It is in effect a text receiving function.
+  // A function for listening for incoming WebUSB tranfers.
+  // This is a recursive function as it has to always listen for
+  // incoming transfers. It is in effect a text receiving function.
   //
   ///////////////////////////////////////////////////////////////////
   let Listen = () => {
     if (!portRef.current) return;
+    const endpointIn =
+      portRef.current.configuration.interfaces[0].alternate.endpoints[0]
+        .endpointNumber;
     portRef.current.transferIn(endpointIn, 64).then((result) => {
       setReciverArea(new TextDecoder().decode(result.data));
-
       Listen(); //recursion
     });
   };
 
   ///////////////////////////////////////////////////////////////////
   //
-  //A function for reading a binary file. Takes the input of the
-  //"choose file" button.
+  // A function for reading a binary file. Takes the input of the
+  // "choose file" button. Used in Read firmware.
   //
   ///////////////////////////////////////////////////////////////////
   const ReadFile = async (input) => {
@@ -90,8 +95,8 @@ export default function Home() {
 
   ///////////////////////////////////////////////////////////////////
   //
-  //This function allows for sending data to the connected
-  //usb device. Data has to be of string type.
+  // This function allows for sending data to the connected
+  // usb device. Data has to be of string type.
   //
   ///////////////////////////////////////////////////////////////////
   const Send = (data) => {
@@ -99,7 +104,9 @@ export default function Home() {
     if (data.length === 0) return;
     console.log("sending to serial:" + data.length);
     console.log("sending to serial: [" + data + "]\n");
-
+    const endpointOut =
+      portRef.current.configuration.interfaces[0].alternate.endpoints[1]
+        .endpointNumber;
     //converting the data into to utf-8 format
     let view = new TextEncoder().encode(data);
     console.log(view);
@@ -114,58 +121,68 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Delta WebUSB App demo</h1>
-      <h2></h2>
-      <div style={{ display: "flex", flexDirection: "column", gap: 5 }}>
+      <h1>Delta WebUSB App</h1>
+      <h4>
+        This website provides a delta firmware update mechanism for Nordic's
+        nrf52480dk board.
+      </h4>
+      <h4>
+        For the board to update its firmware, it needs to have WebUSB handler
+        program in either of its image slots.{" "}
+      </h4>
+      <h4>
+        If the board does not run the handler, click "Install WebUSB handler",
+        and then "Update device".{" "}
+      </h4>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 40,
+        }}
+      >
         <button
           id="connect"
-          onClick={Connect}
+          onClick={GetDevices}
           style={{ visibility: "visible", maxWidth: "max-content" }}
         >
           Connect To WebUSB Device
         </button>
-        <div
-          style={{
-            marginTop: 100,
-          }}
-        >
-          <input type="file" onChange={ReadFile}></input>
+        <button>Install WebUSB handler</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 15,
+          gap: 10,
+        }}
+      >
+        <div>
+          <span style={{ fontWeight: "bold" }}>Product Name: </span>
+          {productName}
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            marginTop: 150,
-          }}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 15,
-          }}
-        >
-          <button id="submit" type="submit" onClick={() => Send(senderArea)}>
-            Send
-          </button>
-          <label>
-            Sender
-            <textarea
-              name="writeMessage"
-              cols={30}
-              onChange={(e) => setSenderArea(e.target.value)}
-              value={senderArea}
-            ></textarea>
-          </label>
-          <label htmlFor="output"> Receiver: </label>
-          <textarea
-            id="output"
-            cols={30}
-            readOnly
-            value={reciverArea}
-          ></textarea>
+        <div>
+          <span style={{ fontWeight: "bold" }}>Manufacturer: </span>
+          {manufacturer}
         </div>
+        <div>
+          <span style={{ fontWeight: "bold" }}>Serial Number: </span>
+          {serialNumber}
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: 15,
+          marginTop: 30,
+        }}
+      >
+        <button>Update device</button>
+        <button>Read Firmware</button>
       </div>
     </>
   );
